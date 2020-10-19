226a3f65dea6feaec315c571a432f5ebbd46e83b
chonglexuanchloe <52901907+chonglexuanchloe@users.noreply.github.com>
2020-10-19 14:56:39 +0800

    Add more options
    sports

--- mycode.py
+++ mycode.py
@@ -4,23 +4,29 @@ print("Welcome to DHS! Please answer the following questions truthfully and we'l
 print("Please respond with a number 1 - 5, where 1 is strongly disagree and 5 is strongly agree.")
 print()
 
-tech1 = input("I enjoy building and fixing things.") 
+tech1 = input("I enjoy building and fixing things.")
 
-outdoor1 = input("I'll go crazy if I do not go out of the house for the whole day.")
+outdoor1 = input("I cannot stand staying in the house.")
 
-music1 = input("I can see colours in my mind when i hear music.")
+music1 = input("I can see colours in my mind when I hear music.")
 
-tech2 = input("I know how to build and operate apps and websites.")
+tech2 = input("I know how to build apps and websites.")
 
-outdoor2 = input("I'm good with building structures.")
+outdoor2 = input("I'm good with tying knots and ropes.")
 
 music2 = input("I play a musical instrument well.")
 
-tech3 = input("I know how to edit codes.")
+tech3 = input("I can code well")
 
-tech_final = int(tech1) + int(tech2)
-outdoor_final = int(outdoor1) + int(outdoor2)
-music_final = int(music1)+ int(music2)
+outdoor3 = input("I enjoy camping")
+
+music3 = input("I can connect with the music")
+
+sports1 = input("I love exercising")
+
+tech_final = int(tech1) + int(tech2)+int(tech3)
+outdoor_final = int(outdoor1) + int(outdoor2)+int(outdoor3)
+music_final = int(music1)+ int(music2)+int(music3)
 
 print()
 
